abfe419207cbd3f6e957c03e8fb3ac3dfc73e14e
Robin Townsend <robin@robin.town>
2022-12-21 00:10:10 -0500

    Add patched versions of Commissioner

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Fonts
 
-This repository contains fonts for the Derani alphabet.
+This repository contains fonts for [Toaq](https://toaq.net), including a font for the [Derani alphabet](https://toaq.me/Derani) and a patched version of [Commissioner](https://fonts.google.com/specimen/Commissioner) for the Latin orthography.
--- flake.nix
+++ flake.nix
@@ -14,14 +14,23 @@
           buildPhase = "fontforge -lang=ff -c 'Open($1); Generate($2); Generate($3); Generate($4)' Derani.sfd Derani.otf Derani.ttf Derani.woff2";
           installPhase = ''
             mkdir -p $out/share/fonts/{opentype,truetype,woff2}
-            mv Derani.otf $out/share/fonts/opentype
-            mv Derani.ttf $out/share/fonts/truetype
-            mv Derani.woff2 $out/share/fonts/woff2
+            mv *.otf $out/share/fonts/opentype
+            mv *.ttf $out/share/fonts/truetype
+            mv *.woff2 $out/share/fonts/woff2
           '';
         };
-      in {
-        defaultPackage = derani-fonts;
-        packages.derani-fonts = derani-fonts;
-      }
+        latin-fonts = pkgs.stdenv.mkDerivation {
+          name = "latin-fonts";
+          src = self;
+          buildInputs = [ pkgs.fontforge-fonttools ];
+          buildPhase = "fontforge -lang=ff -c 'Open($1); Generate($2); Generate($3); Generate($4); Open($5); Generate($6); Generate($7); Generate($8)' \"Commissioner Medium.sfd\" \"Commissioner Medium.otf\" \"Commissioner Medium.ttf\" \"Commissioner Medium.woff2\" \"Commissioner Bold.sfd\" \"Commissioner Bold.otf\" \"Commissioner Bold.ttf\" \"Commissioner Bold.woff2\"";
+          installPhase = ''
+            mkdir -p $out/share/fonts/{opentype,truetype,woff2}
+            mv *.otf $out/share/fonts/opentype
+            mv *.ttf $out/share/fonts/truetype
+            mv *.woff2 $out/share/fonts/woff2
+          '';
+        };
+      in { packages = { inherit derani-fonts latin-fonts; }; }
     );
 }
